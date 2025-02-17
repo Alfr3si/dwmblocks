60e9f068e43ef77ce2efc30d15f69fdba5f513d0
Alfr3si <luisalfredolopez510@gmail.com>
2025-02-17 10:11:59 -0600

    cambio

--- blocks.def.h
+++ blocks.def.h
@@ -1,18 +1,12 @@
-#define SC(ruta) "$HOME/Downloads/dwm/dwmbar/scripts/" #ruta
+#define SC(ruta) "$HOME/.local/share/dwm/dwmblocks/scripts/" #ruta
 //
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
     /*Icon*/    /*Command*/             /*Update Interval*/ /*Update Signal*/
-    {"",	SC("spotify.sh"),	5,	            0  },
-    {"",	SC("mpc.sh"),    	5,	            13 },
-    //{"",	SC("checkupdates.sh"),	3600,               0  },
-    {"",	SC("mail.sh"),      	300,                12 },
-    {"",	SC("volume.sh"),	0,	            10 },
-    {"",	SC("batt.sh"),	        15,	            0  },
-    {"",	SC("clock2"),	        15,	            0  },
-    {"",	SC("memory"),           3,	            0  },
-    {"",	SC("clock2"),	10,	            0  },
-    {"",	SC(""),	        15,	            0  },
+    {"",	SC("memory"),	        3,	    0},
+    {"",	SC("volume"),           0,	    10},
+    {"",	SC("battery"),		10,	    0},
+    {"",	SC("clock2"),	        20,	    0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
--- blocks.h
+++ blocks.h
@@ -3,11 +3,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
     /*Icon*/    /*Command*/             /*Update Interval*/ /*Update Signal*/
-    {"",	SC("memory"),	        3,	            0  },
-    {"",	SC("updates"),	        3600,	            0  },
-    {"",	SC("volume"),           0,	            1  },
-    {"",	SC("battery"),		15,	            0  },
-    {"",	SC("clock2"),	        30,	            0  },
+    {"",	SC("memory"),	        3,	    0},
+    {"",	SC("volume"),           0,	    10},
+    {"",	SC("battery"),		10,	    0},
+    {"",	SC("clock2"),	        20,	    0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
